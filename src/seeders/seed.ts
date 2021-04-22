381af1285580ac3d3c345cf79e94a0dfeee5ed40
AbonyiXavier <francisabonyi@gmail.com>
2021-04-22 11:21:18 +0100

    changed package.json

--- src/seeders/seed.ts
+++ src/seeders/seed.ts
@@ -1,61 +1,60 @@
-import mongoose from 'mongoose';
-import { Roles } from '../models/role.model';
+// import mongoose from 'mongoose';
+// import { Roles } from '../models/role.model';
 
-const url = `${process.env.MONGO_URI}`;
+// const url = `${process.env.MONGO_URI}`;
 
-// connection to mongodb
+// // connection to mongodb
 
-const connect = () => {
-  /** connection mongodb */
-  mongoose
-    .connect(
-      'mongodb+srv://bitcoin:btcoinpass@bitcoin-db.hxqzi.mongodb.net/bitcoin-db?retryWrites=true&w=majority',
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false,
-      }
-    )
-    .then(() => {
-      console.log('mongodb connected...');
-    })
-    .catch((err) => console.log(err.message));
+// const connect = () => {
+//   /** connection mongodb */
+//   mongoose
+//     .connect(
+//       {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//         useCreateIndex: true,
+//         useFindAndModify: false,
+//       }
+//     )
+//     .then(() => {
+//       console.log('mongodb connected...');
+//     })
+//     .catch((err) => console.log(err.message));
 
-  mongoose.connection.on('connected', () => {
-    console.log('Mongoose connected to db');
-  });
-};
-// Drop existing roles if any
-// const roleModelSeed = () => Roles.deleteMany({});
+//   mongoose.connection.on('connected', () => {
+//     console.log('Mongoose connected to db');
+//   });
+// };
+// // Drop existing roles if any
+// // const roleModelSeed = () => Roles.deleteMany({});
 
-const data = [
-  {
-    name: 'user',
-    description: 'User role',
-    ModifiedBy: new Date(),
-    permissions: ['user.view', 'user.manage'],
-  },
-];
-const Seeders = {
-  async seedRoleModel() {
-    try {
-      await Roles.insertMany(data);
-    } catch (error) {
-      console.log('error out', error);
-    }
-  },
-};
+// const data = [
+//   {
+//     name: 'user',
+//     description: 'User role',
+//     ModifiedBy: new Date(),
+//     permissions: ['user.view', 'user.manage'],
+//   },
+// ];
+// const Seeders = {
+//   async seedRoleModel() {
+//     try {
+//       await Roles.insertMany(data);
+//     } catch (error) {
+//       console.log('error out', error);
+//     }
+//   },
+// };
 
-const migration = async () => {
-  try {
-    await connect();
-    // await roleModelSeed();
-    await Seeders.seedRoleModel();
-    console.log('db migration successful');
-  } catch (error) {
-    console.log('error me', error);
-  }
-};
+// const migration = async () => {
+//   try {
+//     await connect();
+//     // await roleModelSeed();
+//     await Seeders.seedRoleModel();
+//     console.log('db migration successful');
+//   } catch (error) {
+//     console.log('error me', error);
+//   }
+// };
 
-migration();
+// migration();
