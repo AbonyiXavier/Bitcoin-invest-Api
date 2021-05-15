b9014e30e8eea36eeed910b48a8c2aad00c71f8c
AbonyiXavier <francisabonyi@gmail.com>
2021-05-15 01:32:03 +0100

    refactor

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -1,14 +1,14 @@
-import dotenv from "dotenv";
-const ejs = require("ejs");
-import createError from "http-errors";
-import path from "path";
+import dotenv from 'dotenv';
+const ejs = require('ejs');
+import createError from 'http-errors';
+import path from 'path';
 dotenv.config();
-const sgMail = require("@sendgrid/mail");
-let key = `${process.env.SENDGRID_API_KEY}`
-let from = `${process.env.FROM_EMAIL}`
+const sgMail = require('@sendgrid/mail');
+let key = `${process.env.SENDGRID_API_KEY}`;
+let from = `${process.env.SENDGRID_USERNAME}`;
 sgMail.setApiKey(key);
 
-export const Mail = async  (options: { mail: any; subject: any; variables: any; email: any; }) => {
+export const Mail = async (options: { mail: any; subject: any; variables: any; email: any }) => {
   try {
     let { mail, subject, variables, email } = options;
     const data = await ejs.renderFile(path.join(__dirname, email), variables, {
@@ -22,7 +22,9 @@ export const Mail = async  (options: { mail: any; subject: any; variables: any; 
     };
     await sgMail.send(msg);
   } catch (error) {
-    console.log("error", error);
-    throw new createError.Conflict(`Request was succesfull, but an Error occured sending confirmation mail`);
+    console.log('error', error);
+    throw new createError.Conflict(
+      `Request was succesfull, but an Error occured sending confirmation mail`
+    );
   }
-}
+};
